264a956d88922a8de7a956e5450280f260259810
Cody Reed <cody2j2reed@gmail.com>
2019-01-04 11:14:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Express!</h1>");
+  // res.send("<h1>Hello Express!</h1>");
+  res.send({
+    name: "Cody",
+    likes: ["cooking", "coding", 333, "being a dad"]
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errMsg: "You have been badded."
+  });
 });
 
 app.listen(3000);
